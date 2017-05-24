2956230b6c03074faf434ae7609cb4914a065fba
chardy <chardy@gmail.com>
2017-05-24 12:53:25 +0800

    Adding basic template and standard css theme with small data set

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="nav">
+      <h1>Care to Vote</h1>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +14,49 @@ export default {
 </script>
 
 <style>
+
+.clearfix:after {
+  visibility: hidden;
+  display: block;
+  font-size: 0;
+  content: " ";
+  clear: both;
+  height: 0;
+}
+.clearfix { display: inline-block; }
+/* start commented backslash hack \*/
+* html .clearfix { height: 1%; }
+.clearfix { display: block; }
+/* close commented backslash hack */
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 44px;
+  padding: 1em 0.5em;
 }
+
+.nav {
+  padding: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10000;
+  background: #FFFFFF;
+  height: 44px;
+  border-bottom: 1px solid #DDD;
+  text-align: center;
+  -moz-box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1), 0px 1px 0px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1), 0px 1px 0px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1), 0px 1px 0px rgba(0, 0, 0, 0.1)
+}
+
+.nav h1 {
+  font-size: 18px;
+  line-height: 18px;
+}
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id='list'>
+    <div class="item clearfix" v-for="item in items" v-component="item">
+      <div class="vote">
+        <div class="total">
+          {{ item.vote }}
+        </div>
+        <div class="action">
+          <a href="#">&#x25B2;</a>
+          <a href="#">&#x25BC;</a>
+        </div>
+      </div>
+      <div class="content">
+        {{ item.content }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        items: [
+          {
+            id: 1,
+            vote: 3,
+            content: 'Google starts tracking offline shopping – what you buy at stores in person ',
+            timestamp: 1495599596265
+          },
+          {
+            id: 2,
+            vote: 1,
+            content: 'Principal Component Analysis Explained Visually (2015) ',
+            timestamp: 1495599790746
+          },
+          {
+            id: 3,
+            vote: 4,
+            content: 'Uber Plans Millions in Back Pay After Shortchanging NYC Drivers',
+            timestamp: 1495599802433
+          },
+          {
+            id: 4,
+            vote: 6,
+            content: 'Dont use Hadoop when your data isnt that big (2013) ',
+            timestamp: 1495599809888
+          },
+          {
+            id: 5,
+            vote: 7,
+            content: 'Porting Windows Dynamic Link Libraries to Linux ',
+            timestamp: 1495599818250
+          },
+          {
+            id: 6,
+            vote: 8,
+            content: 'How a CEO with Dyslexia and ADHD Runs His Company ',
+            timestamp: 1495599825249
+          },
+          {
+            id: 7,
+            vote: 9,
+            content: '22-Year-Old Lidar Whiz Claims Breakthrough',
+            timestamp: 1495599831961
+          },
+          {
+            id: 8,
+            vote: 1,
+            content: 'Daryl Bem and the Replication Crisis',
+            timestamp: 1495599838760
+          },
+          {
+            id: 9,
+            vote: 4,
+            content: ' At The Dawn Of Recorded Sound, No One Cared ',
+            timestamp: 1495599844624
+          },
+          {
+            id: 10,
+            vote: 12,
+            content: 'Jamboard is now available ',
+            timestamp: 1495599850938
+          }
+
+        ]
+      }
+    },
+    components: {
+      item: {
+        computed: {
+          count: () => {
+            return this.upvote - this.downvote
+          }
+        }
+      }
+    }
+  }
+</script>
+<style scoped>
+#list .item {
+  display: block;
+  padding: 5px 0;
+  width: 100vw;
+}
+
+#list .item .vote {
+  width: 60px;
+  border-radius: 5px;
+  border: 1px solid #DDD;
+  float: left;
+}
+
+#list .item .vote .total {
+  line-height: 28px;
+  text-align: center;
+}
+
+#list .item .vote .action {
+  border-top: 1px solid #DDD;
+  text-align: center;
+}
+
+#list .item .vote .action a {
+  display: inline-block;
+  color: #333;
+  width: 20px;
+  text-decoration: none;
+}
+
+#list .item .vote .action a:hover {
+  color: #888;
+}
+
+#list .item .vote .action a:first-child {
+  padding-right: 5px;
+  border-right: 1px solid #DDD;
+}
+
+#list .item .content {
+  padding-left: 0.5em;
+  line-height: 28px;
+  max-width: 80vw;
+  float: left;
+}
+
+</style>
